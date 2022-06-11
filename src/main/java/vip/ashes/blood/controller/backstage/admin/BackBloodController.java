bf8f147aadedf7c1e0085903cfe6780dfeaabc11
loveliness <1695560542@qq.com>
2022-06-11 09:22:53 +0800

    医生抽血

--- src/main/java/vip/ashes/blood/controller/backstage/admin/BackBloodController.java
+++ src/main/java/vip/ashes/blood/controller/backstage/admin/BackBloodController.java
@@ -1,6 +1,7 @@
-package vip.ashes.blood.controller.backstage;
+package vip.ashes.blood.controller.backstage.admin;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import vip.ashes.blood.entity.Blood;
 import vip.ashes.blood.service.BloodService;
@@ -10,20 +11,21 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/backstage/blood")
 public class BackBloodController {
     private BloodService bloodService;
 
     //添加血液
     @PutMapping("insertBlood")
-    public Result insertBlood(@RequestBody Blood blood){
+    public Result insertBlood(@RequestBody Blood blood) {
         boolean save = bloodService.save(blood);
         return Result.ok().success(save);
     }
 
     //全部血液列表
     @PostMapping("/getBloodList")
-    public Result getBloodList(){
+    public Result getBloodList() {
         List<Blood> list = bloodService.list();
         return Result.ok().data(list);
     }
---
+++ src/main/java/vip/ashes/blood/controller/backstage/doctor/DoctorBloodController.java
@@ -0,0 +1,44 @@
+package vip.ashes.blood.controller.backstage.doctor;
+
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vip.ashes.blood.entity.Blood;
+import vip.ashes.blood.service.BloodService;
+import vip.ashes.blood.utils.CurrentUserUtils;
+import vip.ashes.blood.utils.Result;
+
+/**
+ * @author loveliness
+ */
+@RestController
+@AllArgsConstructor
+@Api(tags = "医生血液用户管理")
+@PreAuthorize("hasRole('ROLE_DOCTOR')")
+@RequestMapping("/backstage/doctor/blood")
+public class DoctorBloodController {
+    private final BloodService bloodService;
+    private final CurrentUserUtils currentUserUtils;
+
+    /**
+     * 医生抽血
+     *
+     * @param blood 添加的血液信息
+     * @return Result状态消息
+     */
+    @PutMapping("takeBlood")
+    public Result insertBlood(@RequestBody Blood blood) {
+        //添加抽血人id
+        blood.setTakePerson(currentUserUtils.getCurrentUser().getUserId());
+        boolean save = bloodService.save(blood);
+        if (save) {
+            return Result.ok().message("入库成功");
+        } else {
+            return Result.error().message("入库失败，请联系管理员");
+        }
+    }
+}
