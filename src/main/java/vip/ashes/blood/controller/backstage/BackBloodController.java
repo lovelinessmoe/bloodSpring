595a93f7d239b01a927ff94840f2804b78ce2ffb
jinhang <1422469666@qq.com>
2022-06-11 09:34:59 +0800

    添加注释

--- src/main/java/vip/ashes/blood/controller/backstage/BackBloodController.java
+++ src/main/java/vip/ashes/blood/controller/backstage/BackBloodController.java
@@ -1,5 +1,7 @@
 package vip.ashes.blood.controller.backstage;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import vip.ashes.blood.entity.Blood;
@@ -8,23 +10,52 @@ import vip.ashes.blood.utils.Result;
 
 import java.util.List;
 
+/**
+ * @author jh
+ */
 @RestController
+@Api(tags = "血液管理")
 @AllArgsConstructor
 @RequestMapping("/backstage/blood")
 public class BackBloodController {
     private BloodService bloodService;
 
-    //添加血液
+    /**
+     * 添加一个血液
+     */
     @PutMapping("insertBlood")
-    public Result insertBlood(@RequestBody Blood blood){
+    @ApiOperation(value = "添加血液", notes = "建议协商")
+    public Result insertBlood(@RequestBody Blood blood) {
         boolean save = bloodService.save(blood);
-        return Result.ok().success(save);
+        if (save) {
+            return Result.ok().message("添加成功");
+        } else {
+            return Result.error().message("添加失败");
+        }
     }
 
-    //全部血液列表
+    /**
+     * 全部血液查看
+     */
     @PostMapping("/getBloodList")
-    public Result getBloodList(){
+    @ApiOperation(value = "全部血液查看", notes = "建议协商")
+    public Result getBloodList() {
         List<Blood> list = bloodService.list();
         return Result.ok().data(list);
     }
+
+    /**
+     * 修改血液信息
+     */
+    @PostMapping("/updateBlood")
+    @ApiOperation(value = "修改血液信息", notes = "建议协商")
+    public Result updateBlood(@RequestBody Blood blood) {
+        boolean save = bloodService.updateById(blood);
+        if (save) {
+            return Result.ok().message("成功");
+        } else {
+            return Result.error().message("失败");
+        }
+    }
+
 }
