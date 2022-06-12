6cc66738a56a74e7798e7435b7dbcd0d3a48c2a6
jinhang <1422469666@qq.com>
2022-06-12 16:43:50 +0800

    医生查看自己的申请单（初步完成）

--- src/main/java/vip/ashes/blood/controller/backstage/doctor/DoctorBloodController.java
+++ src/main/java/vip/ashes/blood/controller/backstage/doctor/DoctorBloodController.java
@@ -1,16 +1,19 @@
 package vip.ashes.blood.controller.backstage.doctor;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vip.ashes.blood.entity.Blood;
+import vip.ashes.blood.entity.BloodTransForm;
+import vip.ashes.blood.entity.User;
 import vip.ashes.blood.service.BloodService;
+import vip.ashes.blood.service.BloodTransFormService;
 import vip.ashes.blood.utils.CurrentUserUtils;
 import vip.ashes.blood.utils.Result;
+
+import java.util.List;
 
 /**
  * @author loveliness
@@ -23,7 +26,7 @@ import vip.ashes.blood.utils.Result;
 public class DoctorBloodController {
     private final BloodService bloodService;
     private final CurrentUserUtils currentUserUtils;
-
+    private BloodTransFormService bloodTransFormService;
     /**
      * 医生抽血
      *
@@ -41,4 +44,17 @@ public class DoctorBloodController {
             return Result.error().message("入库失败，请联系管理员");
         }
     }
+
+
+    //医生查看自己的申请单
+    @PostMapping("applyList")
+    public Result applyList() {
+
+        String applyUserId = currentUserUtils.getCurrentUser().getUserId();
+        QueryWrapper<BloodTransForm> wrapper = new QueryWrapper<>();
+        wrapper.eq(BloodTransForm.COL_APPLY_USER, applyUserId);
+        List<BloodTransForm> list = bloodTransFormService.list(wrapper);
+        return Result.ok().data(list);
+    }
+
 }
